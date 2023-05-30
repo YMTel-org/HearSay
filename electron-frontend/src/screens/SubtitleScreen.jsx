f2e619872d1516b5a2dbc1de6df607e39f0c3d3b
tau-bar <taufiq.b.ar@gmail.com>
2023-05-30 10:45:05 +0800

    fix subtitle window design

--- electron-frontend/src/screens/SubtitleScreen.jsx
+++ electron-frontend/src/screens/SubtitleScreen.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect, useRef, useLayoutEffect } from "react";
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Box, Text, Button, useColorMode } from '@chakra-ui/react';
 import { useGlobalState } from "electron-shared-state-react/dist/renderer/useGlobalState";
 
 const SubtitleScreen = () => {
   const [ subtitles, setSubtitles ] = useState("tdsadog dasf asfaf dagag fdga s/397045 5/how-to-make- the-overflow- css-proper ty-work-w ith-hidden-as-value");
   const [transcript, setTranscript] = useGlobalState("transcript");
+  const [theme, setTheme] = useGlobalState("theme", "light");
   const [sentences, setSentences] = useState([]);
   const messagesEndRef = useRef(null);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
@@ -25,9 +27,17 @@ const SubtitleScreen = () => {
     scrollToBottom()
   }, [transcript])
 
+  useEffect(() => {
+    // sync colorMode and theme
+    if (theme !== colorMode) {
+      toggleColorMode();
+    }
+
+  }, [theme]);
+
   return (
-    <Box bg="gray.300" width="fit-content" height="100%" ref={ref} overflow="hidden" style={{ paddingBottom: "50px;" }}>
-      {/* { transcript?  <Text fontSize={16} p={1}>{transcript}</Text> : <Box/>} */}
+    <Box ref={ref} overflow="hidden" style={{ paddingBottom: "50px;", height: "100vh", padding: "20px", color: `${theme === "dark" ? "white" : "black"}` }}>
+      <h2>Subtitles</h2>
       { sentences && sentences.length > 0 && sentences.map(s => <Text fontSize={16} p={1}>{s}</Text>)}
       <div style={{ float:"left", clear: "both", marginTop: "100px" }} ref={messagesEndRef}>
       </div>
